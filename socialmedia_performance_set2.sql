3b808c94ab35646f360b55dea391984e85cab5d0
JanaRakesh <Jana.rakesh23@gmail.com>
2022-03-10 16:32:59 +0000

    Third File

--- socialmedia_performance_set2.sql
+++ socialmedia_performance_set2.sql
@@ -28,7 +28,7 @@ WHERE AGE>15;
 
 --5.find how many percentage of Female users age above 15 use social media?
 
-SELECT ROUND(100.0*SUM(CASE WHEN GENDER='female' THEN 1 ELSE 0 END )/COUNT(*),2) MALE_PERC
+SELECT ROUND(100.0*SUM(CASE WHEN GENDER='female' THEN 1 ELSE 0 END )/COUNT(*),2) FEMALE_PERC
 FROM FACEBOOK
 WHERE AGE>15;
 
