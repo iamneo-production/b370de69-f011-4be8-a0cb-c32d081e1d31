ae67aac71f1ac87addddac01b1a9093edbf2e03e
Harika2901 <kanchiharika22222@gmail.com>
2022-03-10 11:13:23 +0000

    third file

--- performance_3.sql
+++ performance_3.sql
@@ -1,17 +1,29 @@
+--The table name: FACEBOOK
+
 SELECT * FROM FACEBOOK;
+
+--1. Find the total users using social media?
 
 SELECT COUNT(*)
 FROM FACEBOOK;
 
+--2. Find how many Percentage of Male users use social media?
+
 SELECT ROUND(100.0*SUM(CASE WHEN GENDER='male' THEN 1 ELSE 0 END)/COUNT(*),2) MALE_PERC
 FROM FACEBOOK;
+
+--3. Find How many Percentages of female users use social media?
 
 SELECT ROUND(100.0*SUM(CASE WHEN GENDER='female' THEN 1 ELSE 0 END)/COUNT(*),2) FEMALE_PERC
 FROM FACEBOOK;
 
+--4. Find how many Percentage of Male users age above 15 use social media?
+
 SELECT ROUND(100.0*SUM(CASE WHEN GENDER='male' THEN 1 ELSE 0 END)/COUNT(*),2) MALE_PERC
 FROM FACEBOOK
 WHERE AGE>15;
+
+--5. Find how many Percentages of Female users age above 15 use social media?
 
 SELECT ROUND(100.0*SUM(CASE WHEN GENDER='female' THEN 1 ELSE 0 END)/COUNT(*),2) FEMALE_PERC
 FROM FACEBOOK
