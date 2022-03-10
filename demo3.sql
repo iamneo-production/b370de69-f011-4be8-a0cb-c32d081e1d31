72618830a5f334d74e52202d7c62047f6c5e77e6
Pooji582 <18951A05A2@iare.ac.in>
2022-03-10 16:57:56 +0000

    commited3

---
+++ demo3.sql
@@ -0,0 +1,13 @@
+select count(*) from facebook;
+
+select round(100.0*sum(case when gender='male' then 1 else 0 end)/count(*),2)
+male_perc from facebook;
+
+select round(100.0*sum(case when gender='female' then 1 else 0 end)/count(*),2)
+female_perc from facebook; 
+
+select round(100.0*sum(case when gender='male' then 1 else 0 end)/count(*),2)
+male_perc from facebook where age>15;
+
+select round(100.0*sum(case when gender='female' then 1 else 0 end)/count(*),2)
+female_perc from facebook where age>15;
